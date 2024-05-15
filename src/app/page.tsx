132f1aa5722905e6ec2b390d132b9832a9d66c3b
Hycient195 <onyeukwuhycient@gmail.com>
2024-05-15 12:25:43 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/docs" className="btn-black my-auto">Go To Docs</Link>
+      <Link href="/docs" className="btn-black my-auto !py-3 !px-8">Go To Docs</Link>
     </main>
   );
 }
