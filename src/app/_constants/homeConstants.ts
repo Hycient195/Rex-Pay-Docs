a9c3f80e74cc5c170da56a1ef6b40ec190e325a6
Hycient195 <onyeukwuhycient@gmail.com>
2024-06-05 16:16:42 +0100

    fix: landing page

--- src/app/_constants/homeConstants.ts
+++ src/app/_constants/homeConstants.ts
@@ -3,22 +3,22 @@ import sharedImages from "../../_SharedConstants/images";
 
 export const homeConstants = [
   {
-    title: "SDKs and Plugins",
-    subtitle: "Easily integrate RexPay with your platform using any of our SDKs.",
-    image: sharedImages.home1,
-    href: "/docs/sdks-and-plugins/frontend-frameworks"
+    title: "Transfers",
+    subtitle: "Move money from your wallet to bank accounts or other wallets.",
+    image: sharedImages.home3,
+    href: "/"
   },
   {
-    title: "Collect Payments",
+    title: "Accept Payments",
     subtitle: "Collect payments from your customers via card, bank, USSD and many more.",
     image: sharedImages.home2,
     href: "/docs/accept-payments/payment-methods"
   },
   {
-    title: "Transfers",
-    subtitle: "Move money from your wallet to bank accounts or other wallets.",
-    image: sharedImages.home3,
-    href: "/"
+    title: "SDKs and Plugins",
+    subtitle: "Easily integrate RexPay with your platform using any of our SDKs.",
+    image: sharedImages.home1,
+    href: "/docs/sdks-and-plugins/frontend-frameworks"
   },
   {
     title: "Recurring payments",
